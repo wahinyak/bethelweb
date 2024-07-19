8fb7ed9d176768dd3f3e1f5561d5df39f2352c98
evans <evansnt9619@gmail.com>
2024-07-19 09:28:47 +0300

    tailwind installed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,11 @@ import './App.css';
 
 
 function App() {
-return ( <>
-<h1 className='bg-yellow 500 xs'> Hello World</h1>
-</>)
+return ( <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+ <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+</div>)
 }
 
 export default App
