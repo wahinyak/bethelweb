d7ed27c0dc2123a78f60ec48fd4bb9bb05a39c7a
evans <evansnt9619@gmail.com>
2024-07-19 09:29:44 +0300

    tailwindcss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 
 
 function App() {
-return ( <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+return ( <div >
  <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
