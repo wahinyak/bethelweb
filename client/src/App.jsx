1153c19ed15a1cc5cf9d4061a5db5a38a85b2706
evans <evansnt9619@gmail.com>
2024-07-21 23:01:20 +0300

    services

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,21 +3,17 @@ import { Link, Route, Routes } from 'react-router-dom';
 import Home from './components/home';
 import About from './components/about';
 import NotFound from './components/notfound';
+import Services from './components/services';
 
 const App = () => {
   return (
     <div>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </nav>
-      <hr />
+    
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path='/services' element={<Services />} />
       </Routes>
     </div>
   );
