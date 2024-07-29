8cfbe98e060cd52f9ad177a5ee2b05361d60754e
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2024-07-29 19:37:17 +0300

    Integrated events component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/header';
 import Footer from './components/footer';
 import Home from './components/home';
 import About from './components/about';
+import Events from './components/events';
 import NotFound from './components/notfound';
 import Services from './components/services';
 
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/404" element={<NotFound />} />
         <Route path='/services' element={<Services />} />
+        <Route path='/events' element={<Events />} />
       </Routes>
       <Footer />
     </div>
