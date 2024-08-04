2b4d9f86238aad76cc8fec8833a63ecda63a703c
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2024-08-04 22:45:54 +0300

    Replaced background image to use external placeholder image

--- client/src/components/events.jsx
+++ client/src/components/events.jsx
@@ -1,10 +1,8 @@
-import eventsHeader from '../assets/background/Events_header.jpg';
- 
 
 const Events = () => {
   return (
     <div className="flex-1">
-      <section className="text-white h-80 mb-10" style={{backgroundImage: `url(${eventsHeader})`, backgroundSize: "cover"}}>
+      <section className="text-white h-80 mb-10" style={{backgroundImage: "url(https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb)", backgroundSize: "cover"}}>
         <div className="flex items-center justify-center h-full">
           <h1 className="text-4xl font-bold" style={{textDecoration: "underline overline", textDecorationThickness: 4}}>OUR EVENTS</h1>
         </div>
