c856612bca628cbca0d10d74bd2823353137d3b2
wahinyak <wahinyakinaro@gmail.com>
2024-08-09 12:41:52 +0300

    added blog routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
-import { Link, Route, Routes } from 'react-router-dom';
-import Header from './components/header';
-import Footer from './components/footer';
-import Home from './components/home';
+import {Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Homeome';
 import About from './components/about';
-import NotFound from './components/notfound';
+import NotFound from './components/Notfound';
 import Services from './components/services';
+import Bloghome from './blog/pages/Bloghome';
+import Archive from './blog/pages/Archive';
+import Blogpost from './blog/pages/Blogpost';
 
 const App = () => {
   return (
@@ -16,6 +19,9 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/404" element={<NotFound />} />
         <Route path='/services' element={<Services />} />
+        <Route path='/bloghome' element={<Bloghome />} />
+        <Route path='/archive' element={<Archive />} />
+        <Route path='/blogpost' element={<Blogpost />} />
       </Routes>
       <Footer />
     </div>
