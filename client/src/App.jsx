5e4bed2917ed5bcce38143adea353e6dd43c738e
evans <evansnt9619@gmail.com>
2024-08-23 13:35:03 +0300

    dark mode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,48 @@
-import {Route, Routes } from 'react-router-dom';
-import Header from './components/header.jsx'
-import Footer from './components/footer.jsx'
-import Home from './components/home.jsx';
-import About from './components/about.jsx';
-import Events from './components/events.jsx'
-import NotFound from './components/notfound.jsx';
-import Services from './components/services.jsx';
+import { useEffect, useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/header';
+import Footer from './components/footer';
+import Home from './components/home';
+import About from './components/about';
+import Events from './components/events';
+import NotFound from './components/notfound';
+import Services from './components/services';
 import Blogpost from './blog/pages/Blogpost';
 import Bloghome from './blog/pages/Bloghome';
 import Archive from './blog/pages/Archive';
 import './App.css';
 
 function App() {
+  const [theme, setTheme] = useState('cupcake');
+
+  useEffect(() => {
+    const userPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const storedTheme = localStorage.getItem('theme');
+    const initialTheme = storedTheme || (userPrefersDark ? 'night' : 'cupcake');
+
+    setTheme(initialTheme);
+    document.documentElement.setAttribute('data-theme', initialTheme);
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'night' ? 'cupcake' : 'night';
+    setTheme(newTheme);
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+  };
+
   return (
     <div className="appContainer">
-
-      <Header />
+      <Header toggleTheme={toggleTheme} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/404" element={<NotFound />} />
-        <Route path='/services' element={<Services />} />
-        <Route path='/events' element={<Events />} />
-        <Route path='/blog' element={<Bloghome />} />
-        <Route path='/archive' element={<Archive />} />
-        <Route path='/blog/:slug' element={<Blogpost />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/blog" element={<Bloghome />} />
+        <Route path="/archive" element={<Archive />} />
+        <Route path="/blog/:slug" element={<Blogpost />} />
       </Routes>
       <Footer />
     </div>
--- client/src/components/services.jsx
+++ client/src/components/services.jsx
@@ -36,9 +36,9 @@ const servicesData = [
 
 const Services = () => {
   return (
-    <div className="bg-gray-100 text-gray-900 min-h-screen justify-center flex-1">
+    <div className="bg-base-100 text-base-content min-h-screen justify-center flex-1">
       {/* Services Section */}
-      <section className="relative bg-cover bg-full text-white h-80" style={{ backgroundImage: "url('https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb')", backgroundSize: "cover"}}>
+      <section className="relative bg-cover bg-full text-white h-80" style={{ backgroundImage: "url('https://images.pexels.com/photos/976866/pexels-photo-976866.jpeg?auto=compress&cs=tinysrgb')", backgroundSize: "cover" }}>
         <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center">
           <div className="h-1 bg-white w-80 mb-2"></div>
           <h2 className="text-4xl font-bold mb-2">OUR SERVICES</h2>
@@ -52,8 +52,8 @@ const Services = () => {
           <div className="flex items-center my-8">
             <div className="flex-grow border-t border-gray-400"></div>
             <span className="mx-4 text-xl font-bold">Our program</span>
-           <div className="flex-grow border-t border-gray-400"></div>
-        </div>
+            <div className="flex-grow border-t border-gray-400"></div>
+          </div>
           <table className="table-auto w-full md:w-2/3 mx-auto text-left">
             <tbody>
               <tr className="border-b">
@@ -96,19 +96,19 @@ const Services = () => {
       {/* Past Services Section */}
       <section className="py-12">
         <div className="container mx-auto px-4">
-        <div className="flex items-center my-8">
+          <div className="flex items-center my-8">
             <div className="flex-grow border-t border-gray-400"></div>
             <span className="mx-4 text-xl font-bold">Past Services</span>
             <div className="flex-grow border-t border-gray-400"></div>
-        </div>
+          </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {servicesData.map((service, index) => (
-              <div key={index} className="bg-white rounded-lg shadow-md overflow-hidden">
+              <div key={index} className="bg-base-100 rounded-lg shadow-md overflow-hidden">
                 <img src={service.image} alt="Service" className="w-full h-48 object-cover" />
                 <div className="p-4">
                   <h4 className="font-bold text-lg">{service.title}</h4>
-                  <p className="text-sm text-gray-600">{service.pastor} - {service.date}</p>
-                  <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded">Watch</button>
+                  <p className="text-sm text-base-content">{service.pastor} - {service.date}</p>
+                  <button className="mt-4 bg-primary text-primary-content py-2 px-4 rounded">Watch</button>
                 </div>
               </div>
             ))}
@@ -118,5 +118,6 @@ const Services = () => {
     </div>
   );
 };
+
 
 export default Services
