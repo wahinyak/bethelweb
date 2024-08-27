9bbaa5593850600ea00b0de812e28a0164e9b5d0
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2024-08-27 15:00:16 +0300

    Update on carousel structure, reverend image and text

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -26,26 +26,44 @@ const Home = () => {
   };
 
   return (
-    <div className="flex-1 relative">
-      <section className="background-image bg-cover bg-center text-white duration-500" 
+    <div className="flex-1">
+      <section className="bg-cover bg-center text-white duration-500 relative mb-10"
         style={{ 
           backgroundImage: `url(${slides[curr].url})`,
           backgroundRepeat: 'no-repeat',
           width: '100%',
           height: '100vh'
-        }}
-      >
+        }}>
+
+        <div className="absolute inset-0 p-2 flex items-center justify-between">
+          <button onClick={prev} className="text-white bg-black bg-opacity-80 rounded-full p-1 hover:bg-black"><ChevronLeft size={30} /></button>
+          <button onClick={next} className="text-white bg-black bg-opacity-80 rounded-full p-1 hover:bg-black"><ChevronRight size={30} /></button>
+        </div>
+
+        <div className="absolute bottom-2 right-0 left-0 flex justify-center gap-4">
+          {slides.map((slide, slideIdx) =>
+	    <div className="text-xs bg-black rounded-full p-0.5"><FaCircle /></div>)}
+        </div>
       </section>
 
-      <div className="absolute inset-0 p-2 flex items-center justify-between">
-        <button onClick={prev} className="text-white bg-black bg-opacity-80 rounded-full p-1 hover:bg-black"><ChevronLeft size={30} /></button>
-        <button onClick={next} className="text-white bg-black bg-opacity-80 rounded-full p-1 hover:bg-black"><ChevronRight size={30} /></button>
-      </div>
+      <section className="relative bg-cover bg-center"
+        style={{
+	  backgroundImage: `url("https://drive.google.com/thumbnail?id=1PjjaWysqg7WTRnrHDAeeOWxCfDDr9c0t&sz=h600")`,
+	  backgroundRepeat: 'no-repeat',
+          width: '100%',
+          height: '100vh',
+	  }}>
 
-      <div className="absolute bottom-2 right-0 left-0 flex justify-center gap-4">
-        {slides.map((slide, slideIdx) =>
-	  <div className="text-xs"><FaCircle /></div>)}
-      </div>
+        <div className="absolute inset-0 flex flex-col items-center justify-around sm:flex-row">
+          <img src="https://drive.google.com/thumbnail?id=1eoqvPtFKW3ltgY5uUpU0kPPTjeyNxM_Q&sz=h400" alt="Parish Minister"/>
+	  <div className="max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl mx-auto p-2">
+	    <h1 className="text-lg font-bold">Word from our Parish Minister</h1>
+	    <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+	    <h2 className="pt-2 text-2xl font-semibold underline">Rev. Moses</h2>
+	  </div>
+        </div>
+
+      </section>
 
     </div>
   );
