9095a4b0ef7c2a7ef20c1ee1c805ec72811d2e63
evans <evansnt9619@gmail.com>
2025-01-21 05:17:52 +0300

    ruai central added

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bethelweb/',  // Ensure this is set to the correct base path
+   // Ensure this is set to the correct base path
 });
